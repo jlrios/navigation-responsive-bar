45225b93c31ad05418860ecacfb340a4a22d3cd9
JLRD <rdjl5791@gmail.com>
2026-07-03 02:45:36 -0600

    feat: add right-side information panel

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 const menu = document.getElementById("menu");
+const panel = document.querySelector(".info-panel");
+const closeButton = document.querySelector(".panel-close");
+const showPanelButton = document.querySelector(".test-panel-button");
 
+// Menu
 menu.addEventListener("click", () => {
-   document.body.classList.toggle("menu-toggle");
+  document.body.classList.toggle("menu-toggle");
 });
+
+// Test panel
+showPanelButton.addEventListener("click", () => {
+  console.log("Toggle.");
+  panel.classList.add("open");
+});
+
+// Close information panel
+closeButton.addEventListener("click", () => {
+  panel.classList.remove("open");
+});
+
